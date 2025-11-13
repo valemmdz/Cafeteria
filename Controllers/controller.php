a8376bae54508b74c2a9be638038d35f0d2739db
Valeria <vsalasmendez1@gmail.com>
2025-11-13 15:32:12 -0600

    rutas absolutas

--- Controllers/controller.php
+++ Controllers/controller.php
@@ -1,16 +1,22 @@
 <?php
 class MvcController {
+
     public function pagina() {
-        include "Views/template.php";
+        // Ruta absoluta al template
+        include __DIR__ . "/../Views/template.php";
     }
 
     public function enlacesPaginasController() {
+
         if (isset($_GET['action'])) {
             $enlace = $_GET['action'];
         } else {
             $enlace = "inicio";
         }
+
+        // El modelo ya devuelve la ruta completa
         $respuesta = Paginas::enlacesPaginasModel($enlace);
+
         include $respuesta;
     }
 }
--- Models/modelo.php
+++ Models/modelo.php
@@ -1,14 +1,19 @@
 <?php
 class Paginas {
     public static function enlacesPaginasModel($enlace) {
+
         $modulos = ["inicio", "nosotros", "servicios", "contactenos"];
-        
-        // Verificar si el módulo existe físicamente
-        $ruta = "Views/modules/" . $enlace . ".php";
-        if (in_array($enlace, $modulos) && file_exists($ruta)) {
-            return $ruta;
+
+        // Ruta absoluta a la carpeta de módulos
+        $base = __DIR__ . "/../Views/modules/";
+
+        // Ruta del archivo real
+        $archivo = $base . $enlace . ".php";
+
+        if (in_array($enlace, $modulos) && file_exists($archivo)) {
+            return $archivo;
         } else {
-            return "Views/modules/inicio.php";
+            return $base . "inicio.php";
         }
     }
 }
